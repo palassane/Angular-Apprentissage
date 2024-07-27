3c4100b150b031844ced95cf4d0cea3b19ea77ab
palassane <pro4alou@gmail.com>
2024-07-27 21:11:10 +0000

    installation, creation du projet angular

---
+++ prise-en-main/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'This is my first App';
+
+  firstName = 'Bill';
+  lastName = 'Smith';
+}
